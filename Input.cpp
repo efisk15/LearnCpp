c5eeb612f2e6cabc9855c9172c2b66ea04f1bce5
efisk15 <128869985+efisk15@users.noreply.github.com>
2024-01-09 15:12:11 -0500

    Added files on strings and user input.

---
+++ Input.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int x, y;
+    // cout << "Print a number for x: ";
+    // cin >> x;
+    // cout << "Print a number for y: ";
+    // cin >> y;
+    // cout << "The sum of x and y is: " << y+x << endl;
+    // string str = "Hello ";
+    // cout << str << y << " Why";
+
+    // float f1 = 35E3;
+    // double d1 = 12e4;
+    // cout << f1 << endl;
+    // cout << d1;
+
+    // char a = 65, b = 'b';
+    // cout << a << endl << b;
+
+    // string r = "Hello";
+    // cout << r;
+
+    int e = 5;
+    e &= 4;
+    cout << e;
+}
